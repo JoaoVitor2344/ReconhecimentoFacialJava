b7bb2901efd9d81244448a63102d9330298b87ff
João Vitor <52049527+JoaoVitor2344@users.noreply.github.com>
2021-03-25 22:32:35 -0300

    Add files via upload

---
+++ src/Reconhecimento/TesteJavaCV.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reconhecimento;
+
+import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
+import static org.bytedeco.javacpp.opencv_face.createEigenFaceRecognizer;
+
+/**
+ *
+ * @author João Vitor
+ */
+public class TesteJavaCV {
+    public static void main(String args[])
+    {
+        FaceRecognizer r = createEigenFaceRecognizer(); 
+    }
+}
